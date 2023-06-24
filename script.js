6b9bd65abdb64b4e7ed97b9e037bf143ae61989f
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2023-06-24 15:51:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // complete the given function
 
 function palindrome(str){
-
+	let str1 = str.toLowerCase();
+	let i = 0;
+	let j = str.length;
+	while(i<j){
+		if(!(str1.charAt(i)>='a' && str1.charAt(i)<='z')){
+			i++;
+		}
+		else if((!(str1.charAt(j)>='a' && str1.charAt(j)<='z')){
+			j--;
+		}else{
+			if(str1.charAt(i)!=str1.charAt(j)){
+				return false;
+			}
+			i++;
+			j--;
+		}
+	}
+return true;
 }
 module.exports = palindrome
